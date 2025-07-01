cd587f5c3f3ecd253e94068c3871b68b28309f67
RajPurohit-hub <rajpurohit3894@gmail.com>
2025-07-01 15:59:07 +0530

    Add project files.

---
+++ DEMO/Tutorial_2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Tutorial_2
+{
+	public static void Main(string[] args)
+	{
+		Console.WriteLine("NAME:PUROHIT RAJ");
+		Console.WriteLine("DOB:01/11/2005");
+		Console.WriteLine("Address: 4, xyx society,");
+        Console.WriteLine("         tKalawad Road");
+		Console.WriteLine("City: Rajkot");
+		Console.WriteLine("Pincode: 360 001");
+		Console.WriteLine("State: Gujarat");
+		Console.WriteLine("Country: India");
+		Console.WriteLine("Email:rajpurohit3894@gmail.com");
+    }
+}
+
+//City: Rajkot
+
+//Pincode: 360 001
+
+//State: Gujarat
+
+//Country: India
+
+//Email: abc @ymail.com
