192abb2d00186d8bb2b3d65daf1ef7eb1e082e85
RajPurohit-hub <rajpurohit3894@gmail.com>
2025-07-17 13:25:15 +0530

    question - 4 of tutorial 1

---
+++ .net Tutorials/Q-4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEMO
+{
+    internal class Q_4
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("Enter Number : ");
+            string str = Console.ReadLine();
+            x = Convert.ToInt32(str);
+            if (x % 2 == 0)
+            {
+                Console.WriteLine("Number is Odd");
+            }
+            else
+            {
+                Console.WriteLine("Number is Even");
+            }
+                Console.ReadLine();
+        }
+    }
+}
+
+        
